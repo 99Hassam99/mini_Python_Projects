c79b941cc5955116d77c5e614a5656bf9a10cab4
Hassam Ahmad <144841227+99Hassam99@users.noreply.github.com>
2024-10-05 11:35:06 +0500

    Add files via upload

---
+++ The_Perfect_Guess_Game.py
@@ -0,0 +1,17 @@
+import random
+from random import randint
+
+n = random.randint(1,100)
+a = -1
+guesses = 1
+
+while(a != n ):
+    a = int(input("Guess a number: "))
+    if(a>n):
+        print("Lower number please!")
+        guesses += 1
+    elif(a<n):
+        print("Higher number please!")
+        guesses += 1
+
+print(f'You have guessed the number {n} correctly in {guesses} attempts!')
